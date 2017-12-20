13ee57bc645d3de3e4b171012c89ea441a66172a
Edith Mandujano <edithmandujano001@gmail.com>
2017-12-20 12:26:49 -0600

    se agrega carrito y se llaman los id del DOM

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const producto = $("#producto)";
+const imagen= $('#imagen');
+const tituloMochila = $('#titulo-mochila');
+const tituloReverse = $('#titulo-reverse');
+const descripcion = $('#description');
+const marca = $('#marca');
+const modelo = $('#modelo');
